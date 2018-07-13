565bc582df5962a5c249e06c0640aebec5043970
Loki Astari <Loki.Astari@gmail.com>
2018-07-13 01:20:36 -0700

    Formula builds on my machine now

--- Formula/ThorsSerializer.rb
+++ Formula/ThorsSerializer.rb
@@ -1,12 +1,17 @@
 class Thorsserializer < Formula
   desc "Declarative Serialization Library for C++. Serializes in Json/Yaml"
   homepage "https://github.com/Loki-Astari/ThorsSerializer"
-  url "https://github.com/Loki-Astari/ThorsSerializer/raw/ThorsSerializer-1.0.0.tar.gz"
+  url "git@github.com:Loki-Astari/ThorsSerializer.git", :using => :git, :tag => "1.5.1"
+
+  ENV["COV"]  = "gcov"
 
   def install
     system "./configure", "--disable-binary", "--prefix=#{prefix}"
     system "make", "install"
   end
+
+  depends_on "wget"
+  depends_on "cmake"
 
   test do
     system "false"
