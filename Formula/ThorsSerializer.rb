77dbcaa6ab5b9651a2e2206f41172d167db378a4
Loki Astari <Loki.Astari@gmail.com>
2018-07-15 09:04:33 -0700

    Fixed style cop requirements from brew

--- Formula/ThorsSerializer.rb
+++ Formula/ThorsSerializer.rb
@@ -1,17 +1,17 @@
 class GitWithGitAt < GitHubGitDownloadStrategy
   def update_submodules
-    if ! File.exist?(".firstTime")
-        # I use git@github.com for all sub-modules.
-        # We can not assume all users have keys on github so this will not work
-        # The following converts build into an https url. The .configure script
-        # will convert all other sub-modules with the "--with-thor-build-on-travis" flag.
-        # On subsequent calls we can use the standard version of update_submodules
-        system "mv", ".gitmodules", "gitmodules.old"
-        system("sed -e 's#git@\\([^:]*\\):#https://\\1/#' gitmodules.old > .gitmodules")
-        system "git", "submodule", "update", "--init"
-        File.open(".firstTime", "w") {}
+    if !File.exist?(".firstTime")
+      # I use git@github.com for all sub-modules.
+      # We can not assume all users have keys on github so this will not work
+      # The following converts build into an https url. The .configure script
+      # will convert all other sub-modules with the "--with-thor-build-on-travis" flag.
+      # On subsequent calls we can use the standard version of update_submodules
+      system "mv", ".gitmodules", "gitmodules.old"
+      system("sed -e 's#git@\\([^:]*\\):#https://\\1/#' gitmodules.old > .gitmodules")
+      system "git", "submodule", "update", "--init"
+      File.open(".firstTime", "w") {}
     else
-        super
+      super
     end
   end
 end
@@ -21,7 +21,7 @@ class Thorsserializer < Formula
   homepage "https://github.com/Loki-Astari/ThorsSerializer"
   url "https://github.com/Loki-Astari/ThorsSerializer.git", :using => GitWithGitAt, :tag => "1.5.6"
 
-  ENV["COV"]  = "gcov"
+  ENV["COV"] = "gcov"
 
   def install
     system "./configure", "--disable-binary", "--disable-vera", "--with-thor-build-on-travis", "--prefix=#{prefix}"
