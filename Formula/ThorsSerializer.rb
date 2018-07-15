e8e84db6ba8889e555186ec3f2952ef8767422c0
Loki Astari <Loki.Astari@gmail.com>
2018-07-15 00:13:12 -0700

    Update for disabling Vera

--- Formula/ThorsSerializer.rb
+++ Formula/ThorsSerializer.rb
@@ -1,12 +1,24 @@
+class GitWithGitAt < GitHubGitDownloadStrategy
+  def update_submodules
+    if ! File.exist?(".firstTime")
+        system "mv", ".gitmodules", "gitmodules.old"
+        system("sed -e 's#git@\\([^:]*\\):#https://\\1/#' gitmodules.old > .gitmodules")
+        File.open(".firstTime", "w") {}
+    else
+        super
+    end
+  end
+end
+
 class Thorsserializer < Formula
   desc "Declarative Serialization Library for C++. Serializes in Json/Yaml"
   homepage "https://github.com/Loki-Astari/ThorsSerializer"
-  url "git@github.com:Loki-Astari/ThorsSerializer.git", :using => :git, :tag => "1.5.1"
+  url "git@github.com:Loki-Astari/ThorsSerializer.git", :using => GitWithGitAt, :tag => "1.5.2"
 
   ENV["COV"]  = "gcov"
 
   def install
-    system "./configure", "--disable-binary", "--prefix=#{prefix}"
+    system "./configure", "--disable-binary", "--disable-vera", "--prefix=#{prefix}"
     system "make", "install"
   end
 
